9a444671014e373645e7c3da99f90dbe5f9a7a68
Ryan Lucas <55145311+RyanLucas3@users.noreply.github.com>
2023-01-12 01:29:50 -0500

    SurVITE mutual information network

---
+++ importance_weighting.py
@@ -0,0 +1,51 @@
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+
+def at_risk_probability(tr_x, tr_t, tr_a):
+
+    '''
+    :param tr_x: patient covariates.
+    :param tr_t: the time of event realization.
+    :param tr_a: treatment that the patient actually took.
+    :return: the at-risk probability.
+    '''
+
+    means = {} # Finding the average number of individuals at-risk beyond time tau
+
+    # for {1,..., t_max}
+    for τ in range(1, int(max(tr_t))):
+        
+        # find I[t_i ⋝ τ] for all patients i ∈ [n].
+        tau_geq = is_greater_than_tau(tr_t, τ) 
+
+        # find I[t_i ⋝ τ] × I[a_i = 1] for all patients.
+        tau_a = np.array([tr_a[i]*tau_geq[i] for i in range(tr_x.shape[0])]) 
+
+        # Collect average at-risk probability at time τ
+        means[τ] = np.mean(tau_a)
+
+    means = list(means.values())
+    means = [means[0]] + list(means)
+
+    return means
+
+def propensity_score(tr_x, tr_a):
+
+    '''
+    :param tr_x: patient covariates.
+    :param tr_a: treatment that the patient actually took.
+    :return: the propensity score (the probability that a patient is in the treated group).
+    '''
+
+    # Logistic regression predicting P(A = a|X = x),
+    clf = LogisticRegression('l2', class_weight='balanced',C=3.0)
+
+    # Fit and output logistic probabilities.
+    clf.fit(tr_x, tr_a)
+    prob_all = clf.predict_proba(tr_x)
+
+    return prob_all
+
+def is_greater_than_tau(tr_t, tau):
+    # {I[t_i ⋝ τ]: t ∈ tr_t}
+    return np.array([1 if t > tau else 0 for t in tr_t])
---
+++ survite_MI.py
@@ -0,0 +1,65 @@
+import tensorflow.compat.v1 as tf
+tf.disable_eager_execution()
+tf.disable_v2_behavior() 
+# Based on the paper MINE (Mutual Information Neural Estimation): https://arxiv.org/abs/1801.04062.
+# Adapted to SurVITE: https://arxiv.org/pdf/2110.14001.pdf.
+
+class SurVITE_MI:
+
+    def __init__(self, α, H):
+        '''
+        :param α: the penalty term punishing information loss in the representation ɸ.
+        :param H: the number of hidden layers in the network.
+        '''
+
+        # initialize network settings
+        self.α = α
+        self.H = H
+
+
+                
+    def SurVITE_MI_estimator(self, ɸ, X, A):
+
+        '''
+        :param ɸ: latent covariates for patients {1, ..., n} in mini-batch.
+        :param X: observational (at-risk) covariates for patients {1, ..., n} in mini-batch.
+        :param A: vector of assigned treatment for patients {1, ..., n} in mini-batch.
+        :return: θ_k+1: next iteration of SurVITE MI risk.
+        '''
+
+        n = tf.shape(X)[0]
+        
+        # shuffle ɸ to obtain sample draws from the marginal
+        ɸ_shuffle = tf.random_shuffle(ɸ)
+
+        # Respective entries in the vertically aligned tensors 
+        # correspond to the joint distribution and marginals
+        ɸ_conc = tf.concat([ɸ, ɸ_shuffle], axis=0)
+        X_conc = tf.concat([X, X], axis=0)
+        
+        # Forward-pass
+        
+        layerɸ = tf.compat.v1.layers.dense(inputs = ɸ_conc, units = self.H, activation = 'linear')
+        layerX = tf.compat.v1.layers.dense(inputs = X_conc, units = self.H, activation = 'linear')
+        layer2 = tf.nn.relu(layerɸ + layerX)
+        output = tf.compat.v1.layers.dense(inputs = layer2, units = 1, activation = 'linear')
+        
+        # split f_ɸX and f_ɸ_X predictions
+        f_ɸX = output[:n]
+        f_ɸ_X = output[n:]
+        
+        # MI_1 loss: see research task equation 9
+        MI_1 = tf.tensordot(f_ɸX, A, axes = 0) + tf.math.log(
+            tf.tensordot(A, tf.math.exp(f_ɸ_X), axes = 0))
+
+        # MI_0 loss: see research task equation 10
+        MI_0 = tf.tensordot(f_ɸX, 1-A, axes = 0) + tf.math.log(
+            tf.tensordot(1-A, tf.math.exp(f_ɸ_X), axes = 0))
+
+        # L_MI: see research task equation 11
+        # Minus because we're minimizing (we want to maximize MI or equivalently minimize -MI!)
+        L_MI = tf.multiply(-1.0, MI_1 + MI_0)
+
+        optimizer = tf.train.AdamOptimizer(learning_rate=0.01).minimize(L_MI)
+
+        return L_MI, optimizer
